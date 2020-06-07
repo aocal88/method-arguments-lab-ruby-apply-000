c99a42262b26657e16b5422f2873703a26cb0d2b
HAN O <a.ocal88@gmail.com>
2020-06-07 16:41:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,6 @@
 def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hi, my name is #{name} and"
+  puts "I am learning to program in #{language}.\n"
 end
 
 introduction_with_language("Dan", "Ember.js")
