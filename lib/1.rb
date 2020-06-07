e910f0ef5056341c194f40e7858cce6e21617180
HAN O <a.ocal88@gmail.com>
2020-06-07 16:27:15 +0000

    Automatically backed up by Learn

---
+++ lib/1.rb
@@ -0,0 +1,5 @@
+def introduction(name, language)
+  puts "Hi, my name is #{name}. I am learning to program in #{language}."
+end
+
+introduction("Josh", "Ruby")
---
+++ lib/introduction.rb
@@ -0,0 +1,5 @@
+def introduction(name, language)
+  puts "Hi, my name is #{name}. I am learning to program in #{language}.\n"
+end
+
+introduction("Dan", "Ember.js")
